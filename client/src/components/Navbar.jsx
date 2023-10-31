ca062288cf833738305b857d17b42762fd70548f
Darshan <darshanpatel2923@gmail.com>
2023-10-31 19:01:05 +0530

    Navbar, and hero section almost done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,99 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../styles/Navbar.css'
+import Main_Logo from '../assets/PawBits.png'
 
 const Navbar = () => {
-    return (
-        <section id='navbar'>
+    const [nav, setNav] = useState(false);
 
-        </section>
+    const openNav = () => {
+        setNav(!nav);
+    };
+
+    return (
+        <>
+            <nav>
+                {/* mobile */}
+                <div className={`mobile-navbar ${nav ? 'open-nav' : ""} `}>
+                    <div onClick={openNav} className="mobile-navbar__close">
+                        <i className='fa-solid fa-xmark'></i>
+                    </div>
+                    <ul className='mobile-navbar__links'>
+                        <li>
+                            <a onClick={openNav} href='#home'>
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a onClick={openNav} href='#about'>
+                                About
+                            </a>
+                        </li>
+                        <li>
+                            <a onClick={openNav} href='#projects'>
+                                Projects
+                            </a>
+                        </li>
+                        <li>
+                            <a onClick={openNav} href='#contact'>
+                                Contact
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                {/* desktop */}
+
+                <div className="navbar">
+                    <div className="navbar__img">
+                        <a href='/' onClick={() => window.scrollhref(0, 0)}>
+                            <img src={Main_Logo} alt="Logo" style={{ width: "150px", height: "150px" }} />
+                        </a>
+                    </div>
+                    <ul className="navbar__links">
+                        <li>
+                            <a className="home-link" href="#home">
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            {" "}
+                            <a className="about-link" href="#about">
+                                About
+                            </a>
+                        </li>
+                        <li>
+                            {" "}
+                            <a className="contributors-link" href="#contributors">
+                                Star Contributors
+                            </a>
+                        </li>
+                        <li>
+                            {" "}
+                            <a className="register-link" href="#register">
+                                Register
+                            </a>
+                        </li>
+                        <li>
+                            {" "}
+                            <a className="contact-link" href="#projects">
+                                Contact
+                            </a>
+                        </li>
+                        <li>
+                            {" "}
+                            <a className="adopt-link" href="#about">
+                                <div className="adopt_button">
+                                    Adopt Now
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                    <div className="mobile-hamb" onClick={openNav}>
+                        <i className="fa-solid fa-bars-staggered"></i>
+                    </div>
+                </div>
+            </nav>
+        </>
     )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import Jeffery_dog from '../assets/arlington-dog-boarding.webp'
+import '../styles/Home.css'
+import bg_img from '../assets/main_bg_img.png'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <>
+            <div class="demo-wrap">
+                <img
+                    class="demo-bg"
+                    src={bg_img}
+                    alt="bg_img"
+                />
+                <section class="main_section">
+                    <div className="main_container">
+                        <div className="main_content">
+                            <div className="main_text">
+                                <div className="main_text_header">Help Save Jeffery</div>
+                                <div className="main_text_subheader">Join Hands And Be A Guardian </div>
+                                <div className="main_text_para">Find your furry partner according to your preferences and help us create better for them. </div>
+                                <div className="adopt_button">
+                                    <a className="adopt-link" href="#about">
+                                        Adopt Now
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="main-image">
+                                <img src={Jeffery_dog} alt="main-img" />
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </>
     )
 }
 
